b000793f5b13dfd81920997ef936ce2132c254a7
k-nagata-2026 <carekarte-dev2@care-next.jp>
2026-06-03 14:30:49 +0900

    Create Player class with name and hp attributes

---
+++ Player.java
@@ -0,0 +1,13 @@
+public class Player {
+    String name;
+    int hp;
+
+    public Player(String name, int hp) {
+        this.name = name;
+        this.hp   = hp;
+    }
+
+    public void receiveDamage() {
+        this.hp -= 1;
+    }
+}
